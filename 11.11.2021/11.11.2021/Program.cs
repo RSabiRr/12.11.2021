99fa3691720bf03e37fc01dde1770ba373a36ce9
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-11-12 12:01:50 +0400

    Add files via upload

---
+++ 11.11.2021/11.11.2021/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11._11._2021
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task4
+            //List<car> cars = new List<car>();
+            //car car1 = new car();
+            //car1.Model = "BMW";
+            //car car2 = new car();
+            //car2.Model = "Mercedes";
+            //car car3 = new car();
+            //car3.Model = "VAZ";
+            //car car4 = new car();
+            //car4.Model = "Hunday";
+            //car car5 = new car();
+            //car5.Model = "Kia";
+
+            //cars.Add(car1);
+            //cars.Add(car2);
+            //cars.Add(car3);
+            //cars.Add(car4);
+            //cars.Add(car5);
+
+
+            //foreach (var item in cars)
+            //{
+            //    Console.WriteLine($"{item.Model}");
+            //}
+            #endregion
+            #region task5
+            //List<byte> Ages = new List<byte>();
+            //Ages.Add(15);
+            //Ages.Add(20);
+            //Ages.Add(30);
+            //List<byte> ages1 = new List<byte>();
+            //ages1.AddRange(Ages);
+            //foreach (var item in ages1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("**********************");
+            //Ages.Remove(15);
+            //foreach (var item in Ages)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("*****************");
+            ////Ages.RemoveAll(e => e > 15);
+            //Console.WriteLine("************");
+            ////Ages.RemoveAt(1);
+            //Console.WriteLine("************");
+            ////Ages.RemoveRange(1, 2);
+
+            //Ages.ForEach(item => Console.WriteLine(item));
+            #endregion
+        }
+    }
+}
---
+++ 11.11.2021/11.11.2021/car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._11._2021
+{
+    class car
+    {
+        public string Brend { get; set; }
+        public string Model { get; set; }
+        public string DoorCount { get; set; }
+
+        public static List<car> Fullinfo()
+        {
+
+            return car.Fullinfo();
+        }
+
+    }
+}
